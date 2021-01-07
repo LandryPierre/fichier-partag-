aef1e91cdbaa5d628db40e84cd2def5119fe239b
LandryPierre <pierre.landry1109@gmail.com>
2021-01-07 11:08:55 -0400

    updt 4

--- Index.php
+++ Index.php
@@ -1,11 +1,5 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Fichier Partagé</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="style.css">
-        <script type="text/javascript" src="script.js"></script>
-    </head>
     <body>
         
     </body>
---
+++ head.php
@@ -0,0 +1,6 @@
+<head>
+    <title>Fichier Partagé</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="script.js"></script>
+</head>
