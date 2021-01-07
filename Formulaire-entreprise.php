0d7830f0b46b25a43bbe7ffdb9fd20ff1af6f0a1
NON-dWeb <noemienino94@gmail.com>
2021-01-07 16:11:17 -0400

    champs différente parties fiche entreprise

--- Formulaire-entreprise.php
+++ Formulaire-entreprise.php
@@ -1,47 +1,69 @@
 <h1>Fiche Entreprise</h1>
+<form>
 <section>
     <div class=1>
         <p>Logo</p>
+        <input type="file">
     </div>
 </section>
 <section>
     <div>
-        <p>Adresse</p>
+    <p>Adresse postale</p>
+        <input type="text" placeholder="Adresse" name="text">
+    </div>
+    <div>
+        <input type="text" placeholder="Région/Département" name="text">
+    </div>
+    <div>
+    <input type="text" placeholder="Commune" name="text">
+    </div>
+    <div>
+    <input type="text" placeholder="Code Postal" name="text">
     </div>
     </section>
     <div>
         <p>Dénomination</p>
+        <input type="text" name="text">
     </div>
  <section>
 <section>
 <div>
     <p>Site Web</p>
+        <input type="text" placeholder="Soumettre le lien" name="text">
     </div>
 </section>
 <section>
 <div>
     <p>Numéros de téléphone</p>
+    <input type="text" placeholder="Téléphone mobile" name="text">
+   <br> <input type="text" placeholder="Téléphone fixe" name="text">
     </div>
 </section>
 <section>
 <div>
     <p>Liste du personnel</p>
+    <ul>
+    <li><input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+    <li><input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+    <li><input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+    <li><input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+    <li><input type="text" name="text"></li>
+   <li> <input type="text" name="text"></li>
+</ul>
     </div>
 </section>
-<section>
-<div>
-    <p>Employé 1</p>
-    </div>
-</section>
-<section>
-<div>
-    <p>Employé 2</p>
-    </div>
-</section>
-<section>
-<div>
-    <p>Employé 3</p>
-    </div>
-</section>
-
+</form>
 
