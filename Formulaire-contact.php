1bb81842bcdefbc7bba69bce0e05fd92a63a5a4e
LandryPierre <pierre.landry1109@gmail.com>
2021-01-07 16:32:43 -0400

    modification formulaire

--- Formulaire-contact.php
+++ Formulaire-contact.php
@@ -1,21 +1,40 @@
 <h1>Fiche Contact</h1>
+
 <section>
-    <div class=1>
-        <p>Logo</p>
-    </div>
     <div class=2>
         <p>Photo</p>
+        <input type="file">
     </div>
 </section>
+
 <section>
     <div>
-        <p>Adresse</p>
+        <p>Adresse postale</p>
+        <input type="text" placeholder="Adresse" name="text">
     </div>
     <div>
-        <p>Numéros de téléphone</p>
+        <input type="text" placeholder="Région/Département" name="text">
+    </div>
+    <div>
+    <input type="text" placeholder="Commune" name="text">
+    </div>
+    <div>
+    <input type="text" placeholder="Code Postal" name="text">
     </div>
 </section>
+
+<section>
+<div>
+    <p>Numéros de téléphone</p>
+    <input type="text" placeholder="Téléphone mobile" name="text"><br>
+    <input type="text" placeholder="Téléphone fixe" name="text">
+</div>
+</section>
+
 <section>
     <p>Commentaires contact</p>
     <p>Date/ Lieu/ Commentaire</p>
+    <input type="text" placeholder="Commentaires contact" name="text"><br>
+    <input type="date"><br>
+    <textarea name="text"></textarea>
 </section>
