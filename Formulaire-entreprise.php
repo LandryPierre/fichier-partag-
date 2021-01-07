e94d9d041b5ff829f5d2556a0e0ecfdfc35943c0
NON-dWeb <noemienino94@gmail.com>
2021-01-07 15:21:05 -0400

    création fiche entreprise

---
+++ Formulaire-entreprise.php
@@ -0,0 +1,47 @@
+<h1>Fiche Entreprise</h1>
+<section>
+    <div class=1>
+        <p>Logo</p>
+    </div>
+</section>
+<section>
+    <div>
+        <p>Adresse</p>
+    </div>
+    </section>
+    <div>
+        <p>Dénomination</p>
+    </div>
+ <section>
+<section>
+<div>
+    <p>Site Web</p>
+    </div>
+</section>
+<section>
+<div>
+    <p>Numéros de téléphone</p>
+    </div>
+</section>
+<section>
+<div>
+    <p>Liste du personnel</p>
+    </div>
+</section>
+<section>
+<div>
+    <p>Employé 1</p>
+    </div>
+</section>
+<section>
+<div>
+    <p>Employé 2</p>
+    </div>
+</section>
+<section>
+<div>
+    <p>Employé 3</p>
+    </div>
+</section>
+
+
