43a1f86599ceb032d9008653531f3574622d9650
NON-dWeb <noemienino94@gmail.com>
2021-01-07 16:54:03 -0400

    ajouter éléments sur et centrer formulaire entreprise

--- Formulaire-entreprise.php
+++ Formulaire-entreprise.php
@@ -1,69 +1,50 @@
-<h1>Fiche Entreprise</h1>
+<h1 style='text-align: center'>Fiche Entreprise</h1>
 <form>
 <section>
-    <div class=1>
+    <div class=1 style='text-align: center'>
         <p>Logo</p>
         <input type="file">
     </div>
 </section>
 <section>
-    <div>
+    <div style='text-align: center'>
     <p>Adresse postale</p>
         <input type="text" placeholder="Adresse" name="text">
     </div>
-    <div>
+    <div style='text-align: center'>
         <input type="text" placeholder="Région/Département" name="text">
     </div>
-    <div>
+    <div style='text-align: center'>
     <input type="text" placeholder="Commune" name="text">
     </div>
-    <div>
+    <div style='text-align: center'>
     <input type="text" placeholder="Code Postal" name="text">
     </div>
     </section>
-    <div>
+    <div style='text-align: center'>
         <p>Dénomination</p>
         <input type="text" name="text">
     </div>
  <section>
 <section>
-<div>
+<div style='text-align: center'>
     <p>Site Web</p>
         <input type="text" placeholder="Soumettre le lien" name="text">
     </div>
 </section>
 <section>
-<div>
+<div style='text-align: center'>
     <p>Numéros de téléphone</p>
     <input type="text" placeholder="Téléphone mobile" name="text">
    <br> <input type="text" placeholder="Téléphone fixe" name="text">
+   <br> <input type="text" placeholder="Fax" name="text">
+   <br> <input type="text" placeholder="E-mail" name="text">
+</div>
+  
+</section> 
+ <div style='text-align: center'>
+    <br>
+    <input type="submit" value="soumettre">
     </div>
-</section>
-<section>
-<div>
-    <p>Liste du personnel</p>
-    <ul>
-    <li><input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-    <li><input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-    <li><input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-    <li><input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-    <li><input type="text" name="text"></li>
-   <li> <input type="text" name="text"></li>
-</ul>
-    </div>
-</section>
 </form>
 
