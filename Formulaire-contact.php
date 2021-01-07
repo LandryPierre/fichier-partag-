a694cd0a729280144fab9a471bacc5a0a7dc33b6
LandryPierre <pierre.landry1109@gmail.com>
2021-01-07 14:50:46 -0400

    ajout formulaire

---
+++ Formulaire-contact.php
@@ -0,0 +1,21 @@
+<h1>Fiche Contact</h1>
+<section>
+    <div class=1>
+        <p>Logo</p>
+    </div>
+    <div class=2>
+        <p>Photo</p>
+    </div>
+</section>
+<section>
+    <div>
+        <p>Adresse</p>
+    </div>
+    <div>
+        <p>Numéros de téléphone</p>
+    </div>
+</section>
+<section>
+    <p>Commentaires contact</p>
+    <p>Date/ Lieu/ Commentaire</p>
+</section>
